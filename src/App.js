e6ec2f7fc7fb3649462a82812b71e3d2c72d64b8
Luis M. García <luismago@gmail.com>
2020-04-10 16:27:32 +0200

    add header to the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from './components/layout/Header';
+
+import './App.css';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div style={{ height: '1500px', background: 'cornflowerblue' }}>
+      <Header />
+      <div>Hello</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,59 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import React, { useEffect, useState } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SideDrawer from './SideDrawer';
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isTransparent, setIsTransparent] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsTransparent(false);
+      } else {
+        setIsTransparent(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: isTransparent ? 'transparent' : '#2f2f2f',
+          boxShadow: 'none',
+          padding: '10px 0',
+          transition: '.5s',
+        }}
+      >
+        <Toolbar>
+          <div className="header_logo">
+            <div className="font_righteous header_logo_venue">The venue</div>
+            <div className="header_logo_title">Musical Events</div>
+          </div>
+
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => setIsOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <SideDrawer open={isOpen} onClose={() => setIsOpen(false)} />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Header;
